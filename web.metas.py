f291e1764a3ec5c6b6138d05c6a22f891083c78e
joaopimentel28p-cell <joaopimentel28p@gmail.com>
2025-11-23 10:21:02 -0300

    Update web.metas.py

--- web.metas.py
+++ web.metas.py
@@ -1,5 +1,3 @@
-Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import sqlite3
 from datetime import date
@@ -168,3 +166,4 @@ st.subheader("Adicionar Meta")
             st.warning("Meta removida!")
 
         st.markdown("---")
+
